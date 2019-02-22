23fc5a8d90b8071979de6c5fccd51dad3dd105d3
Seanba <sean@seanba.com>
2019-02-21 21:52:23 -0600

    Appearing block scripts added

---
+++ SuperPrefabReplacement/Assets/ThisExerciseFiles/AppearingBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AppearingBlock : MonoBehaviour
+{
+    public int m_GroupNumber;
+
+#if UNITY_EDITOR
+    // Only allow editor scripts to set our group number property
+    // This is going to be set by custom properties from Tiled Objects in our Tiled map file
+    public int GroupNumber
+    {
+        set
+        {
+            m_GroupNumber = value;
+        }
+    }
+#endif
+}
+
---
+++ SuperPrefabReplacement/Assets/ThisExerciseFiles/AppearingBlockManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AppearingBlockManager : MonoBehaviour
+{
+    private List<AppearingBlock> m_AppearingBlocks = new List<AppearingBlock>();
+
+    private void Start()
+    {
+        // Gather our list of appearing blocks
+        // And start each block off as inactive/invisible
+        foreach (var go in GameObject.FindGameObjectsWithTag("AppearingBlock"))
+        {
+            go.SetActive(false);
+            var block = go.GetComponent<AppearingBlock>();
+            if (block != null)
+            {
+                m_AppearingBlocks.Add(block);
+            }
+        }
+
+        StartCoroutine(ManageBlocks());
+    }
+
+    private IEnumerator ManageBlocks()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        // Cycle through our groups
+        while (true)
+        {
+            // Group 0 now active
+            foreach (var block in m_AppearingBlocks)
+            {
+                block.gameObject.SetActive(block.m_GroupNumber == 0);
+            }
+
+            yield return new WaitForSeconds(2.0f);
+
+            // Group 1 now active
+            foreach (var block in m_AppearingBlocks)
+            {
+                block.gameObject.SetActive(block.m_GroupNumber == 1);
+            }
+
+            yield return new WaitForSeconds(2.0f);
+
+            // Group 2 now active
+            foreach (var block in m_AppearingBlocks)
+            {
+                block.gameObject.SetActive(block.m_GroupNumber == 2);
+            }
+
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+}
+
